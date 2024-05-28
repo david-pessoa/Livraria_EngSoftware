f33b93783908200486bcf4ef80d20b43727ff77a
joao <joaopedrosouzagg27@gmail.com>
2024-05-28 19:15:24 -0300

    realizando testes de reserva

--- demo/src/test/java/com/example/ReservaTest.java
+++ demo/src/test/java/com/example/ReservaTest.java
@@ -1,7 +1,33 @@
 package com.example;
 
-public class ReservaTest {
-    
+import java.time.LocalDate;
 
-    
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ReservaTest {
+
+    @Test
+    public void calculaTotalReserva() {
+        Livro livroTeste = new Livro("LivroTeste", 99.99f, "Autor", "Categoria");
+        int quantidadeTeste = 3;
+        Reserva reservaTeste = new Reserva(livroTeste, quantidadeTeste);
+
+        double totalEsperado = livroTeste.getPreco() * quantidadeTeste;
+        double totalCalculado = reservaTeste.calculaTotalReserva();
+
+        Assert.assertEquals(totalEsperado, totalCalculado, 0.001);
+    }
+
+    @Test
+    public void dataReservaDeveSerAtual() {
+        Livro livroTeste = new Livro("LivroTeste", 99.99f, "Autor", "Categoria");
+        int quantidadeTeste = 3;
+        Reserva reservaTeste = new Reserva(livroTeste, quantidadeTeste);
+
+        LocalDate dataAtual = LocalDate.now();
+        LocalDate dataReserva = reservaTeste.getDataReserva();
+
+        Assert.assertEquals(dataAtual, dataReserva);
+    }
 }
